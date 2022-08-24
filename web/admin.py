90e61bc65ff72fdabcb6e7413c0bb01c246821df
MRprgmr <algoprogram01@gmail.com>
2022-08-25 01:03:21 +0500

    Seach fields were added

--- web/admin.py
+++ web/admin.py
@@ -7,4 +7,5 @@ from web.models import Certificate
 class CertificateAdmin(admin.ModelAdmin):
     list_display = ['full_name', 'certificate_number', 'given_date', 'source_image', 'certificate_download']
     readonly_fields = ['certificate_id', 'source_image', 'certificate_download']
+    search_fields = ['certificate_number', 'certificate_id']
     list_per_page = 20
