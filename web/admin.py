66a075d33c2e8f807daa440d90e401de8f0e69a5
MRprgmr <algoprogram01@gmail.com>
2022-08-25 23:23:15 +0500

    Some changes

--- web/admin.py
+++ web/admin.py
@@ -7,5 +7,15 @@ from web.models import Certificate
 class CertificateAdmin(admin.ModelAdmin):
     list_display = ['full_name', 'certificate_number', 'given_date', 'source_image', 'certificate_download']
     readonly_fields = ['certificate_id', 'source_image', 'certificate_download']
-    search_fields = ['certificate_number', 'certificate_id']
+    search_fields = ['certificate_number']
     list_per_page = 20
+
+    def get_search_results(self, request, queryset, search_term):
+        queryset, use_distinct = super(CertificateAdmin, self).get_search_results(request, queryset, search_term)
+        try:
+            search_term_as_int = int(search_term)
+        except ValueError:
+            pass
+        else:
+            queryset = self.model.objects.filter(certificate_number=search_term_as_int)
+        return queryset, use_distinct
